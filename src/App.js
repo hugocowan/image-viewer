2bd1db27d4fccb47f95eab615ffedcdedbcef9b7
hugocowan <hugocowan915@gmail.com>
2019-07-01 21:25:29 +0100

    Converted App to classical component. Added some more styling and better spacing.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,47 @@
 import React from 'react';
 import './App.scss';
 
-function App() {
+class App extends React.Component {
 
-  const images = require.context('./assets', false, /\.(png|jpe?g|gif)$/)
-    .keys()
-    .map(imageLink => imageLink.replace('./', ''));
+  constructor(props) {
+    super(props);
+    this._app = null;
 
-  const first = Math.ceil((images.length - 1) / 3), scnd = images.length - 1 - first;
+    this.state = {
+      images: require.context('./assets', false, /\.(png|jpe?g|gif)$/).keys()
+        .map(imageLink => imageLink.replace('./', ''))
+    };
 
-  return (
-    <div className="App">
-      <div className='img-wrapper left'>
-        {images.slice(0, first).map(imageLink => 
-        <img prop={imageLink} alt={`From ${imageLink}`} key={imageLink} src={require(`./assets/${imageLink}`)} />
-        )}
+    
+  }
+  
+  render() {
+
+    console.log(this.state);
+
+    const { images } = this.state,
+      first = Math.ceil((images.length - 1) / 3), scnd = images.length - 1 - first;
+  
+    return (
+      <div className="App" ref={c => this._app = c}>
+        <div className='img-wrapper left'>
+          {images.slice(0, first).map(imageLink => 
+          <img prop={imageLink} alt={`From ${imageLink}`} key={imageLink} src={require(`./assets/${imageLink}`)} />
+          )}
+        </div>
+        <div className='img-wrapper center'>
+          {images.slice(first + 1, scnd).map(imageLink => 
+          <img prop={imageLink} alt={`From ${imageLink}`} key={imageLink} src={require(`./assets/${imageLink}`)} />
+          )}
+        </div>
+        <div className='img-wrapper right'>
+          {images.slice(scnd + 1, images.length - 1).map(imageLink => 
+          <img prop={imageLink} alt={`From ${imageLink}`} key={imageLink} src={require(`./assets/${imageLink}`)} />
+          )}
+        </div>
       </div>
-      <div className='img-wrapper center'>
-        {images.slice(first + 1, scnd).map(imageLink => 
-        <img prop={imageLink} alt={`From ${imageLink}`} key={imageLink} src={require(`./assets/${imageLink}`)} />
-        )}
-      </div>
-      <div className='img-wrapper right'>
-        {images.slice(scnd + 1, images.length - 1).map(imageLink => 
-        <img prop={imageLink} alt={`From ${imageLink}`} key={imageLink} src={require(`./assets/${imageLink}`)} />
-        )}
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
