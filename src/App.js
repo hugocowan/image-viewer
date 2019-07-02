4bb4903a1538180fbc2060ba19cd64aa0e27a010
hugocowan <hugocowan915@gmail.com>
2019-07-02 14:27:52 +0100

    Cleaned up and added comments to code.

--- src/App.js
+++ src/App.js
@@ -2,27 +2,16 @@ import React from 'react';
 import ImageRender from './components/ImageRender';
 import './App.scss';
 
-class App extends React.Component {
+function App() {
 
-    constructor(props) {
-        super(props);
+    const images = require.context('./assets', false, /\.(png|jpe?g|gif)$/).keys()
+        .map(imageLink => imageLink.replace('./', ''));
 
-        this.state = {
-            images: require.context('./assets', false, /\.(png|jpe?g|gif)$/).keys()
-                .map(imageLink => imageLink.replace('./', ''))
-        };
-    }
-
-    render() {
-
-        const { images } = this.state;
-
-        return (
-            <div className="App">
-                <ImageRender images = {images}/>
-            </div>
-        );
-    }
+    return (
+        <div className="App">
+            <ImageRender images = {images}/>
+        </div>
+    );
 }
 
 export default App;
--- src/components/ImageRender.js
+++ src/components/ImageRender.js
@@ -9,90 +9,130 @@ class ImageRender extends React.Component {
             twoThirds = props.images.length - 1 - oneThird;
 
         this.state = {
-            splits: [ 0, oneThird, twoThirds, props.images.length - 1 ]
+
+            // Split the image array into three equal columns
+            columns: [
+                props.images.slice(0, oneThird),
+                props.images.slice(oneThird, twoThirds),
+                props.images.slice(twoThirds, props.images.length - 1)
+            ],
+            sorted: false,  
         };
+
+        // Declare colHeight vars here so they can be referenced anywhere.
+        this.col0Height = 0;
+        this.col1Height = 0;
+        this.col2Height = 0;
     }
     
+    // Ensure image column heights <= biggest image height:
+    // Wait for images to load.
+    // Make copies of each column array so we aren't manipulating state constantly.
+    // Get individual image sizes from each column, and the size of the biggest image.
+    // If diff between column heights > max image height, move an image.
+    // Move image from biggest column to smallest column.
+    // Update the new column sizes.
+    // Check again until the column height difference <= max image height.
+    // Once the columns are leveled out, set state with the new column arrays.
+    onImgLoad = () => {
 
-    onImgLoad = (col, index, end) => {
+        const _this = this, columns = [ this.col0.children, this.col1.children, this.col2.children ];
 
-        if (col === 2 && index === end) {
+        setTimeout(() => {
+
+            // Make a copy of the image columns, initialise other vars.
+            let [ col0, col1, col2 ] = [ ..._this.state.columns ],
+                maxHeight = 0,
+                col0Heights = [],
+                col1Heights = [],
+                col2Heights = [];
             
-            
-            
-            function getHeights(_this) {
-                
-                setTimeout(() => {
+            // Get individual image sizes for each column array, in order. Also get max image height.
+            columns.forEach((col, index) => {
+                for (let i = 0; i < col.length; i++) {
                     
-                    let col0Height = _this.col0.clientHeight,
-                        col1Height = _this.col1.clientHeight,
-                        col2Height = _this.col2.clientHeight;
+                    index === 0 ? col0Heights.push(col[i].clientHeight) :
+                    index === 1 ? col1Heights.push(col[i].clientHeight) :
+                                    col2Heights.push(col[i].clientHeight);
 
-                    const avgHeight = (col0Height + col1Height + col2Height) / _this.props.images.length;
+                    if (maxHeight < col[i].clientHeight) maxHeight = col[i].clientHeight;
+                }
+            });
+            
+            // Get the current height of each column by adding up the image heights.
+            _this.col0Height = col0Heights.reduce((a, b) => a + b);
+            _this.col1Height = col1Heights.reduce((a, b) => a + b);
+            _this.col2Height = col2Heights.reduce((a, b) => a + b);
 
-                    const splits = [..._this.state.splits];
-                    
-                    if (col0Height - col1Height > avgHeight) {
+            // While diff between columns > max image height, keep moving images.
+            while ( Math.abs(_this.col0Height - _this.col1Height) > maxHeight ||
+                    Math.abs(_this.col1Height - _this.col2Height) > maxHeight ||
+                    Math.abs(_this.col2Height - _this.col0Height) > maxHeight  ) 
+            {
+                // Find the largest column to take an image from.
+                switch(Math.max(_this.col0Height, _this.col1Height, _this.col2Height)) {
 
-                        splits[1]--;
-                        _this.setState({ splits }, () => {
-                            col0Height = _this.col0.clientHeight;
-                            col1Height = _this.col1.clientHeight;
-                            if (col0Height - col1Height > avgHeight) getHeights(_this);
-                        });
-                    } else if (col0Height - col1Height < avgHeight) {
+                    case _this.col0Height: 
+                        findSmallestColumn(col0, col0Heights, 'col0Height');
+                        break;
 
-                        splits[1]++;
-                        _this.setState({ splits }, () => {
-                            col0Height = _this.col0.clientHeight;
-                            col1Height = _this.col1.clientHeight;
-                            if (col0Height - col1Height < avgHeight) getHeights(_this);
-                        });
-                    }
+                    case _this.col1Height: 
+                        findSmallestColumn(col1, col1Heights, 'col1Height');
+                        break;
 
-                    if (col1Height - col2Height > avgHeight) {
-
-                        splits[2]--;
-                        _this.setState({ splits }, () => {
-                            col1Height = _this.col1.clientHeight;
-                            col2Height = _this.col2.clientHeight;
-                            if (col1Height - col2Height > avgHeight) getHeights(_this);
-                        });
-                    } else if (col1Height - col2Height < avgHeight) {
-                        
-                        splits[2]++;
-                        _this.setState({ splits }, () => {
-                            col1Height = _this.col1.clientHeight;
-                            col2Height = _this.col2.clientHeight;
-                            if (col1Height - col2Height < avgHeight) getHeights(_this);
-                        });
-                    }
-
-                }, 100);
+                    default:
+                        findSmallestColumn(col2, col2Heights, 'col2Height');
+                        break;
+                }
             }
 
-            getHeights(this);
-        }
+            function findSmallestColumn(col, colHeights, colHeight) {
+                
+                // Find the smallest column to add an image to.
+                switch(Math.min(_this.col0Height, _this.col1Height, _this.col2Height)) {
+
+                    case _this.col0Height:
+                        moveImage(col0, 'col0Height', col0Heights, col, colHeights, colHeight);
+                        break;
+
+                    case _this.col1Height:
+                        moveImage(col1, 'col1Height', col1Heights, col, colHeights, colHeight);
+                        break;
+
+                    default:
+                        moveImage(col2, 'col2Height', col2Heights, col, colHeights, colHeight);
+                        break;
+                }
+            }
+
+            // Move image from larger to smaller column, update column and image heights.
+            function moveImage(colA, colAHeight, colAHeights, colB, colBHeights, colBHeight) {
+
+                colA.push(colB.pop());
+                colAHeights.push(colBHeights.pop());
+                _this[colAHeight] = colAHeights.reduce((a, b) => a + b);
+                _this[colBHeight] = colBHeights.reduce((a, b) => a + b);
+            }
+
+            // Set state with the finalised column arrays.
+            _this.setState({ columns: [ col0, col1, col2 ], sorted: true });
+
+        }, 1000);
     }
 
     render() {
 
-        const { images } = this.props, 
-            { splits } = this.state;
-
         return (
             <div className='img-container'>
-                {[0, 1, 2].map(col => {
+                {this.state.columns.map((col, i) => {
 
-                    return <div key={col} className='img-wrapper' ref={c => this[`col${col}`] = c}>
-                        {images.slice(splits[col], splits[col + 1]).map((imageLink, index, arr) =>
+                    return <div key={col} className='img-wrapper' ref={c => this[`col${i}`] = c}>
+                        {col.map((imageLink, index, arr) =>
                             <img
                                 key={imageLink}
                                 alt={`From ${imageLink}`}
                                 src={require(`../assets/${imageLink}`)}
-                                onLoad={() => {
-                                    this.onImgLoad(col, index, arr.length - 1);
-                                }}
+                                onLoad={() => !this.state.sorted && i === 2 && index === arr.length - 1 && this.onImgLoad()}
                             />
                         )}
                 </div>})}
